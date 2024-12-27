57dd3f5cce99b0b38e136c2b51d4354013339d59
ZahraThiaw <fatimatathiaw6@gmail.com>
2024-12-27 13:25:47 +0000

    update 1

--- src/main/java/org/example/zahra/springauthentification/SpringAuthentificationApplication.java
+++ src/main/java/org/example/zahra/springauthentification/SpringAuthentificationApplication.java
@@ -2,12 +2,13 @@ package org.example.zahra.springauthentification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class SpringAuthentificationApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAuthentificationApplication.class, args);
 	}
-
 }
+
