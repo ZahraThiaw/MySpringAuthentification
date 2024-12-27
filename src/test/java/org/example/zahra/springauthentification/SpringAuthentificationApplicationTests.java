2293fd566fe2f54b395874d71e10f9bbc9eb9606
ZahraThiaw <fatimatathiaw6@gmail.com>
2024-12-27 12:39:51 +0000

    update

--- src/test/java/org/example/zahra/springauthentification/SpringAuthentificationApplicationTests.java
+++ src/test/java/org/example/zahra/springauthentification/SpringAuthentificationApplicationTests.java
@@ -3,11 +3,11 @@ package org.example.zahra.springauthentification;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SpringAuthentificationApplicationTests {
-
+@SpringBootTest(classes = SpringAuthentificationApplication.class)
+public class SpringAuthentificationApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
+
+
